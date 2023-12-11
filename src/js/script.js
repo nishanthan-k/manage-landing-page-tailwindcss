002aad1ae30d94c73e92a57601072c4bab017093
nishanthan-k <nishanthank9102@gmail.com>
2023-12-11 20:54:16 +0530

    Updated the body

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,8 @@
-// const toggleBtn = document.getElementById("menu-btn");
-// const nav = document.getElementById("menu");
+const toggleBtn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
-// toggleBtn.addEventListener('click', () => {
-//   toggleBtn.classList.toggle('open');
-//   nav.classList.toggle('flex');
-//   nav.classList.toggle('hidden');
-// })
-
-console.log("123456")
+toggleBtn.addEventListener('click', () => {
+  toggleBtn.classList.toggle('open');
+  nav.classList.toggle('flex');
+  nav.classList.toggle('hidden');
+})
