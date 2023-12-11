e884ddf64d94618de9917c0fa3798b844b49dd9e
nishanthan-k <nishanthank9102@gmail.com>
2023-12-11 20:23:16 +0530

    Completed the design

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
+
+toggleBtn.addEventListener('click', () => {
+  toggleBtn.classList.toggle('open');
+  nav.classList.toggle('flex');
+  nav.classList.toggle('hidden');
+})
